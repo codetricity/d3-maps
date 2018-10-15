f408d9c1ead29c075b41d90fc472cb8914ebb078
Craig Oda <craig@oppkey.com>
2018-10-14 21:27:03 -0700

    more exit and enter practice

--- js/practice.js
+++ js/practice.js
@@ -19,20 +19,39 @@ items
 
 
 items = ol.selectAll('li')
-   .data(flowers);
-
-items
+  .data(flowers)
   .text(d => d)
   .exit().remove();
 
-// function updateData(items, dataset) {
+items = ol.selectAll('li')
+  .data(animals)
+  .text(d => d)
+  .enter()
+  .append('li')
+  .text(d => d);
+
+
+items = ol.selectAll('li')
+.data(flowers)
+.text(d => d)
+.exit().remove()
+.enter()
+.append('li')
+.text(d => d);
+
+// updateData(ol, flowers);
+// updateData(ol, animals);
+
+// function updateData(ol, dataset) {
+//   let items = ol.selectAll('li');
+
 //   items
-//     // .text(d => d)
-//     .exit().remove()
 //     .data(dataset)
+//     .text(d => d)
 //     .enter()
 //     .append('li')
-//     .text(d => d);
+//     .text(d => d)
+//     .exit().remove();
 // }
-console.log(items);
+// console.log(items);
   

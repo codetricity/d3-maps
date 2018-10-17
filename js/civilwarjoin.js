a54790cd934e0b2f53d0811c8f86c90df6a9c206
Craig Oda <craig@oppkey.com>
2018-10-17 05:43:12 -0700

    updated pattern

--- js/civilwarjoin.js
+++ js/civilwarjoin.js
@@ -44,21 +44,22 @@ d3.csv('data/civil-war.csv').then((data) => {
 function listAlliance(allianceData) {
   /**
    * Pattern:
-   *  1. data join
-   *  2. exit and remove excess information on screen
-   *  3. update information on screen
-   *  4. enter
-   *  5. append new items
+   *  1. select
+   *  2. data join
+   *  3. exit and remove excess information on screen
+   *  4. update information on screen
+   *  5. enter
+   *  6. append new items
    */
-  const allianceText = svg.selectAll('text')
-  .data(allianceData);  // 1. data join
+  const allianceText = svg.selectAll('text')  // 1. select
+  .data(allianceData);  // 2. data join
 
-  allianceText.exit().remove();   // 2. exit and remove excess information on screen
+  allianceText.exit().remove();   // 3. exit and remove excess information on screen
 
-  allianceText.text(d => d); // 3. update information on screen
+  allianceText.text(d => d); // 4. update information on screen
 
-  allianceText.enter()  // 4. enter and return list of information that needs to be on screen
-    .append('text')     // 5. append
+  allianceText.enter()  // 5. enter and return list of information that needs to be on screen
+    .append('text')     // 6. append
     .text((d) => d)
     .attr('x', '50')
     .attr('y', (d, i) => i * 20 + 50);
